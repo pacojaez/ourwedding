f5987c859ca90e9e71384f5fd293a957c4d99c79
Paco Jaez <pacojaez@gmail.com>
2022-01-28 15:54:21 +0100

    added custom fonts

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -5,21 +5,21 @@
         </h2> --}}
     </x-slot>
     {{-- <x-slot name="content"> --}}
-    <div class="flex flex-col justify-center items-center py-4 sm:w-5/6 xl:w-full">
-        <h2 class="text-4xl font-bold leading-none text-green-900 xl:text-6xl font-abhaya-libre">
+    <div class="flex flex-col justify-center items-center py-2 sm:w-5/6 xl:w-full">
+        <h2 class="text-2xl font-bold leading-none text-green-900 lg:text-4xl font-abhaya-libre">
             ENS CASEM!!!!
         </h2>
-        <h1 class="m-2 font-bold leading-none text-purple-900 text-4xl lg:text-6xl font-dancing">
+        <h1 class="mb-4 font-bold leading-none text-purple-900 text-3xl lg:text-6xl font-dancing">
            ¡olga + paco!
         </h1>
-        <h2 class="text-4xl font-bold leading-none text-green-900 xl:text-6xl font-abhaya-libre">
+        <h2 class="mb-4 font-bold leading-none text-green-900 text-2xl lg:text-4xl font-abhaya-libre">
             NOS CASAMOS!!!!
         </h2>
-        <p class="mt-6 tracking-wider text-center text-gray-700 xl:text-lg font-alegraya-sans">
+        <p class="mb-0 p-2 text-center text-gray-700 xl:text-lg font-alegraya-sans">
             Si has recibido un mail con un usuario y una contraseña, accede al login donde podrás ver toda la info de la boda....
         </p>
         <a href="{{ route('login')}}"
-            class="px-6 py-2 m-6 text-center text-white uppercase bg-green-900 rounded-full shadow-lg font-montserrat sm:font-xl sm:py-4 sm:px-8 hover:bg-green-800 sm:mt-16">
+            class="p-2 text-center text-white uppercase bg-green-900 rounded-full shadow-lg font-montserrat sm:font-xl sm:py-4 sm:px-8 hover:bg-green-800 sm:mt-16">
             LOGIN
         </a>
     </div>
