1917f3f5090463e3b08b79c24d250031e92f68a0
Paco Jaez <pacojaez@gmail.com>
2022-10-17 09:58:59 +0200

    added adresses table to model

--- app/Models/Adress.php
+++ app/Models/Adress.php
@@ -9,6 +9,13 @@ class Adress extends Model
 {
     use HasFactory;
 
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'adresses';
+
     protected $fillable = [
         'direccion',
     ];
